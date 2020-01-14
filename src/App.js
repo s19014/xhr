a3dd9a0965df67f7b8ec698534cf7c301207247a
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-14 17:05:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Greeting = props => (
+    <h1>{props.type}</h1>
+)
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+      return (
+          <div>
+              <Greeting type="good" />
+          </div>
+      )
+  }
+    componentDidMount () {
+        fetch('./messages.json').then(res => res.json()).then(console.log)
+    }
 }
 
-export default App;
+export default App
