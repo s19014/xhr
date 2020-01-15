f5c59ada97d5b8fdb8993ae6ba7e1c0ae2a51ce4
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-15 14:39:47 +0900

    追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
-const Greeting = props => <h1>{props.type}</h1>
+const Greeting = props => <li>{props.type}</li>
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = { data: [] }
   }
 
   render () {
-    return (
-      <div>
-        <Greeting type='good' />
-      </div>
-    )
+    const greetings = this.state.data.map((v, i) => (
+      <Greeting type={v} key={i} />
+    ))
+    return <ul>{greetings}</ul>
   }
   componentDidMount () {
     fetch('./messages.json')
       .then(res => res.json())
-      .then(console.log)
+      .then(json => json.messages)
+      .then(json => this.setState({ data: json }))
   }
 }
 
